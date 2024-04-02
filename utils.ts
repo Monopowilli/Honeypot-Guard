cd1f04c3a33cde33daebb814803dca2303be493d
 Monopowilli <Monopolytoken@mail.com >
2024-04-02 12:00:00 +0800

    feat(classes): added conditional class function to apply classes based on conditions

--- utils.ts
+++ utils.ts
@@ -7,3 +7,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Conditionally returns a class string.
+ */
+export function conditionalClass(
+  condition: boolean,
+  trueClass: string,
+  falseClass: string = ""
+): string {
+  return condition ? trueClass : falseClass;
+}
