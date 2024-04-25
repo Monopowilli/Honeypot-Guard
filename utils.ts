c4943f3feb1344a46f0b7f65aad4cf7d1c980bb5
Monopowilli <Monopolytoken@mail.com>
2024-04-25 12:00:00 +0800

    feat(state): added Redux Toolkit for enhanced state management

--- utils.ts
+++ utils.ts
@@ -4,10 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Web3Provider } from "./contexts/Web3Context"; 
+import { Web3Provider } from "./contexts/Web3Context";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import { toast } from "react-hot-toast"; // Added for toast notifications
+import { store } from "./store"; // Redux store using Redux Toolkit
 
 const queryClient = new QueryClient();
 
@@ -57,5 +56,3 @@ root.render(
 if (process.env.NODE_ENV === "development") {
   reportWebVitals(console.log);
 }
-
-toast.success("App loaded successfully!"); // Show success toast on page load
