ba26b11a752b0a98624699145a6373a27210197d
 Monopowilli <Monopolytoken@mail.com >
2025-04-07 16:16:38 +0800

    feat(dashboard): initialize LiveDashboard component

--- app/components/LiveDashboard.tsx
+++ app/components/LiveDashboard.tsx
@@ -1,4 +1,12 @@
 "use client"
 
-// ... rest of the component code
+import React from 'react'
 
+export default function LiveDashboard() {
+  return (
+    <div>
+      <h1>Live Dashboard</h1>
+      {/* Initialize your dashboard content here */}
+    </div>
+  )
+}
