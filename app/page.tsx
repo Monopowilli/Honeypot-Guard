232c18a49c4391835687dd73643fba2ffa7d1b5e
Monopowilli <Monopolytoken@mail.com>
2025-05-20 10:00:00 +0800

    chore: initial setup with dynamic imports for components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react"
 import dynamic from "next/dynamic"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-  
+
 const DynamicHero = dynamic(() => import("../components/Hero"), { ssr: false })
 const DynamicDashboard = dynamic(() => import("../components/Dashboard"), { ssr: false })
 const DynamicWhyApexify = dynamic(() => import("../components/WhyApexify"), { ssr: false })
@@ -30,4 +30,3 @@ export default function Home() {
     </main>
   )
 }
-
